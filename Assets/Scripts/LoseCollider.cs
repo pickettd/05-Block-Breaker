b9b119a2d0ceb1f15aff2e759b6192a92621d49f
David Pickett <pickettd@gmail.com>
2018-04-30 10:53:34 -1000

    Add 2nd

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -7,7 +7,7 @@ public class LoseCollider : MonoBehaviour {
 	
 	void OnTriggerEnter2D (Collider2D trigger) {
 		levelManager = GameObject.FindObjectOfType<LevelManager>();
-		levelManager.LoadLevel("Loose Screen");
+		levelManager.LoadLevel("Win Screen");
 	}
 	
 	void OnCollisionEnter2D (Collision2D collision) {
